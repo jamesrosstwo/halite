efa32aabc269a2720e39f5b720bbf5052296c476
James <james.rosstwo@gmail.com>
2020-08-06 23:25:09 -0400

    complete board parsing

--- src/board/board.py
+++ src/board/board.py
@@ -23,8 +23,19 @@ class HaliteBoard(Board):
         self.dims = tuple(self.settings["size"])
         self.player_list = {id: HalitePlayer.from_player(p) for id, p in self.players.items()}
         self.player = self.player_list[self.current_player_id]
+
+        self._ordered_player_ids = self.calculate_p_id_list()
+
         self.board = self.parse_board()
 
+    def calculate_p_id_list(self):
+        """
+        Calculates the ordered player ID list. Used for parsing the board.
+        :return: A list of all player IDs with the agent's player ID at index 0
+        """
+        ids = set(self.player_list.keys())
+        ids.remove(self.current_player_id)
+        return [self.current_player_id] + list(ids)
 
     def parse_board(self):
         out_array = np.zeros(self.dims)
@@ -38,7 +49,15 @@ class HaliteBoard(Board):
         # Bound halite between 0 and 1
         out[0] = cell.halite / self.settings["max_cell_halite"]
 
+        if cell.ship_id is not None:
+            ship_idx = self._ordered_player_ids.index(cell.ship_id) + 1
+            out[ship_idx] = 1
 
+        if cell.shipyard_id is not None:
+            shipyard_idx = self._ordered_player_ids.index(cell.shipyard_id) + 5
+            out[shipyard_idx] = 1
+
+        return out
 
     def list_pos_to_board_pos(self, list_pos: int) -> Point:
         """
@@ -52,4 +71,4 @@ class HaliteBoard(Board):
 if __name__ == "__main__":
     a = np.zeros((10, 10, 10))
 
-    a[:,1,1] = [i for i in range(10)]
+    a[:, 1, 1] = [i for i in range(10)]

4051a0da1d74f7c0c3c51f9cb67383c8933ab217
James <james.rosstwo@gmail.com>
2020-08-07 19:04:48 -0400

    add helper functions and type hints

--- src/board/board.py
+++ src/board/board.py
@@ -1,5 +1,5 @@
 from kaggle_environments.envs.halite.helpers import Board, Configuration, Point, Cell
-from typing import Dict, Any, Union
+from typing import Dict, Any, Union, List, Tuple, Optional
 import numpy as np
 from src.constants import SETTINGS
 
@@ -13,6 +13,10 @@ def pos_difference(from_pos: Point, to_pos: Point) -> Point:
     return Point(to_pos.x - from_pos.x, to_pos.y - from_pos.y)
 
 
+def pos_from_indices(indices: Tuple[int, int]):
+    return Point(indices[0], indices[1])
+
+
 class HaliteBoard(Board):
     def __init__(self, board: Dict[str, Any], config: Union[Configuration, Dict[str, Any]]):
         from src.entities.player import HalitePlayer
@@ -23,12 +27,10 @@ class HaliteBoard(Board):
         self.dims = tuple(self.settings["size"])
         self.player_list = {id: HalitePlayer.from_player(p) for id, p in self.players.items()}
         self.player = self.player_list[self.current_player_id]
-
         self._ordered_player_ids = self.calculate_p_id_list()
-
         self.board = self.parse_board()
 
-    def calculate_p_id_list(self):
+    def calculate_p_id_list(self) -> List[int]:
         """
         Calculates the ordered player ID list. Used for parsing the board.
         :return: A list of all player IDs with the agent's player ID at index 0
@@ -37,27 +39,53 @@ class HaliteBoard(Board):
         ids.remove(self.current_player_id)
         return [self.current_player_id] + list(ids)
 
-    def parse_board(self):
+    def parse_board(self) -> np.ndarray:
         out_array = np.zeros(self.dims)
         cells = self.cells
         for cell in cells.values():
             out_array[:, cell.position.y, cell.position.x] = self.parse_cell(cell)
         return out_array
 
-    def parse_cell(self, cell: Cell):
-        out = [0 for _ in range(self.dims[0])]
+    def parse_cell(self, cell: Cell) -> List[float]:
+        out = [0. for _ in range(self.dims[0])]
         # Bound halite between 0 and 1
         out[0] = cell.halite / self.settings["max_cell_halite"]
 
         if cell.ship is not None:
             ship_idx = self._ordered_player_ids.index(cell.ship.player_id) + 1
-            out[ship_idx] = 1
+            out[ship_idx] = 1.
 
         if cell.shipyard is not None:
             shipyard_idx = self._ordered_player_ids.index(cell.shipyard.player_id) + 5
-            out[shipyard_idx] = 1
+            out[shipyard_idx] = 1.
 
         return out
+
+    def ship_map(self, player_id) -> np.ndarray:
+        return self.board[player_id + 1, :, :]
+
+    def ship_at_pos(self, pos: Point) -> Optional['HaliteShip']:
+        cell = self.cells[pos]
+        ship = cell.ship
+        if ship is not None:
+            return HaliteShip.from_ship(ship)
+
+    def shipyard_at_pos(self, pos: Point) -> Optional['HaliteShipyard']:
+        cell = self.cells[pos]
+        shipyard = cell.shipyard
+        if shipyard is not None:
+            return HaliteShipyard.from_shipyard(shipyard)
+
+    def shipyard_map(self, player_id) -> np.ndarray:
+        return self.board[player_id + 5, :, :]
+
+    @property
+    def opponent_ships(self) -> List['HaliteShip']:
+        return np.flatten([p.ships for p in self.opponents])
+
+    @property
+    def opponent_shipyards(self) -> List['HaliteShipyard']:
+        return np.flatten([p.shipyards for p in self.opponents])
 
     def list_pos_to_board_pos(self, list_pos: int) -> Point:
         """
@@ -67,6 +95,9 @@ class HaliteBoard(Board):
         p = divmod(list_pos, self.size)
         return Point(p[1], p[0])
 
+
+from src.entities.ship import HaliteShip
+from src.entities.shipyard import HaliteShipyard
 
 if __name__ == "__main__":
     a = np.zeros((10, 10, 10))

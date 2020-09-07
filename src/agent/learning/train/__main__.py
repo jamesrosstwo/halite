ee4690f689142a835488b7e17920c162d694d8f5
James <james.rosstwo@gmail.com>
2020-09-06 19:59:43 -0700

    add template for halite train agent

--- src/agent/learning/train/__main__.py
+++ src/agent/learning/train/__main__.py
@@ -1,12 +1,21 @@
 from kaggle_environments import make
 from src.constants import ROOT_PATH, SETTINGS
-from src.agent.submission_agent import halite_agent
+
+from typing import Dict, Any
+from src.agent.agent import HaliteAgent
+
+
+def train_agent(observation: Dict[str, Any], configuration: Dict[str, Any]) -> Dict[str, str]:
+    agent = HaliteAgent(observation, configuration)
+    actions = agent.act()
+    return actions
+
 
 print("Making environment")
 env = make("halite", debug=True)
 
 print("Running agent")
-env.run([halite_agent] * 4)
+env.run([train_agent] * 4)
 
 print("Saving replay")
 render_options = SETTINGS["replay"]
--- src/halite_gym/__init__.py
+++
@@ -1,9 +0,0 @@
-from gym.envs.registration import register
-from src.constants import SETTINGS
-
-version_id = "halite-" + SETTINGS["gym"]["version"]
-
-register(
-    id=version_id,
-    entry_point='src.halite_gym.envs:HaliteEnv',
-)
